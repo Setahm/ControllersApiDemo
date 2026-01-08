4e0eb36734f07a2a796d3eabcf5a8b19bb565aca
Setah M <102811063+Setahm@users.noreply.github.com>
2026-01-08 10:59:59 +0300

    Add CustomersController

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ControllersApiDemo.Models;
+
+namespace ControllersApiDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private static List<Customer> customers = new();
+
+        [HttpPost]
+        public IActionResult AddCustomer(Customer customer)
+        {
+            customers.Add(customer);
+            return Ok(customer);
+        }
+
+        [HttpGet]
+        public IActionResult GetCustomers()
+        {
+            return Ok(customers);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCustomer(int id)
+        {
+            var customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null) return NotFound();
+            customers.Remove(customer);
+            return Ok();
+        }
+    }
+}
