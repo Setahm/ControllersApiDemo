a53aa2c53fc81ec24e23d227830a652dd449b67a
Setah M <102811063+Setahm@users.noreply.github.com>
2026-01-08 11:01:10 +0300

    Add ProductsController

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ControllersApiDemo.Models;
+
+namespace ControllersApiDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> products = new();
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            products.Add(product);
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(products);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null) return NotFound();
+            products.Remove(product);
+            return Ok();
+        }
+    }
+}
